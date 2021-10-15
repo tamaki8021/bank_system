cd292b09e46b1f9af4c824c50de0d92d17994c88
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-15 23:56:39 +0900

    :sparkles: web api

--- bankapi/main.go
+++ bankapi/main.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"fmt"
-
+	"os"
+	"log"
+	"net/http"
+	"strconv"
 	"tamaki8021/bank_system/bankcore"
 )
 
+var accounts = map[float64]*bankcore.Account{}
+
 func main()  {
-	fmt.Println(bankcore.Hello())
+	// 口座情報の初期化
+	accounts[1001] = &bankcore.Account{
+		Customer: bankcore.Customer{
+			Name: "John",
+			Address: "Los Angeles, California",
+			Phone: "(213) 555 0147",
+		},
+		Number: 1001,
+	}
+
+	http.HandleFunc("/statement", statement)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func statement(w http.ResponseWriter, req *http.Request) {
+	// 口座番号の取得
+	numberqs := req.URL.Query().Get("number")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Invalid account number!")
+		return 
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			fmt.Sprintf(os.Stdout, account.Statement())
+		}
+	}
+
 }

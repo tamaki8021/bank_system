e5aca69f88b9a97ddeefde4e2ab7176263567dde
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-16 01:20:46 +0900

    fix: error message

--- bankcore/bank.go
+++ bankcore/bank.go
@@ -30,7 +30,7 @@ func (a *Account) Deposit(amount float64) error {
 // 引き出し
 func (a *Account) Withdraw(amount float64) error {
 	if amount <= 0 {
-		return errors.New("the amount to deposit should be greater than zero")
+		return errors.New("the amount to withdraw should be greater than the account's balance")
 	}
 
 	a.Balance -= amount

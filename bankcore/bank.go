49ee6a22ce30ea5b85fd7aa8022b811d9b57f1f7
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-15 22:52:45 +0900

    :sparkles: deposite function

--- bankcore/bank.go
+++ bankcore/bank.go
@@ -1,5 +1,7 @@
 package bankcore
 
+import "errors"
+
 type Customer struct {
 	Name string
 	Address string
@@ -10,4 +12,13 @@ type Account struct {
 	Customer
 	Number int32
 	Balance float64
+}
+
+func (a *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to deposit should be greater than zero")
+	}
+
+	a.Balance += amount
+	return nil
 }
